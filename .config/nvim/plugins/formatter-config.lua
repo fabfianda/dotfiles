703d04eec6b88d823b2eec0935276e5f8d6012f0
fabfianda <fabrizio.fiandanese@gmail.com>
2021-03-20 09:21:02 +0100

    feat: add vue.js formatter

--- .config/nvim/plugins/formatter-config.lua
+++ .config/nvim/plugins/formatter-config.lua
@@ -11,6 +11,16 @@ require('formatter').setup({
           }
         end
     },
+    vue = {
+        -- prettier
+       function()
+          return {
+            exe = "prettier",
+            args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0), '--single-quote'},
+            stdin = true
+          }
+        end
+    },
     rust = {
       -- Rustfmt
       function()
