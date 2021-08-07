0fca4ca7a9f54414ddd1574c54d0b61da45df961
fab <fabrizio.fiandanese@gmail.com>
2021-08-07 21:39:50 +0200

    refactor: minimal telescope-config

--- .config/nvim/plugins/telescope-config.lua
+++ .config/nvim/plugins/telescope-config.lua
@@ -1,23 +1,48 @@
-local actions = require('telescope.actions')
+local actions = require("telescope.actions")
 
-require('telescope').setup {
+require("telescope").setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        -- To disable a keymap, put [map] = false
+        -- So, to not map "<C-n>", just put
+        -- ["<c-x>"] = false,
+
+        -- Otherwise, just set the mapping to the function that you want it to be.
+        -- ["<C-i>"] = actions.select_horizontal,
+
+        -- Add up multiple actions
+        ["<CR>"] = actions.select_default + actions.center
+
+        -- You can perform as many actions in a row as you like
+        -- ["<CR>"] = actions.select_default + actions.center + my_cool_custom_action,
+      },
+      n = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous
+        -- ["<esc>"] = actions.close,
+        -- ["<C-i>"] = my_cool_custom_action,
+      }
+    }
+  },
   extensions = {
     fzf = {
-      fuzzy = true,                    -- false will only do exact matching
+      fuzzy = true, -- false will only do exact matching
       override_generic_sorter = false, -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case" -- or "ignore_case" or "respect_case"
+      -- the default case_mode is "smart_case"
     }
   }
 }
 
-require('telescope').load_extension('fzf')
-
+require("telescope").load_extension("fzf")
 
 -- Global remapping
 ------------------------------
-      -- '--color=never',
+-- '--color=never',
 -- require('telescope').setup{
 --   defaults = {
 --     vimgrep_arguments = {
